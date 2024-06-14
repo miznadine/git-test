dce4d04fca328721878bf7481dd06fa3406fa4d5
Nadine Oland <130494826+miznadine@users.noreply.github.com>
2024-06-13 19:06:56 -0500

    python_change

---
+++ change.py
@@ -0,0 +1,13 @@
+#import sys
+
+dollars = 1
+quarters = 1
+dimes = 1
+nickels = 1
+pennies = 1
+
+def change(do, qu, di, ni, pe):
+    total = (1 * do) + (0.25 * qu) + (0.1 * di) + (0.05 * ni) + (0.01 * pe)
+    print(f"The total value of your change is ${total:.2f}")
+    
+change(dollars, quarters, dimes, nickels, pennies)
